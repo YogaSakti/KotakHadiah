78be85df5fb3e316ec8ce45c4c34e1d1db28a27e
Imperial Owl <yogazacty@gmail.com>
2022-05-19 22:53:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ const BaseApi = Buffer.from('aHR0cHM6Ly9zLmlzYWZlcGFsLmNvbS9zYXBpL3YxL2dpZnRib3g
     const event = await fetch(`${BaseApi}/lists`, { method: 'GET', headers }).then((res) => res.json())
 
     const eventLists = event.data
-    console.log(eventLists);
-    const liveEvents = eventLists.find((data) => data.title == 'Coming Soon')
+    const liveEvents = eventLists.find((data) => data.title == 'Coming Soon' || data.tittle == 'Ongoing Event(s)')
     if (!liveEvents) return log.error('No Event!');
 
     const eventData = liveEvents.data.reverse()

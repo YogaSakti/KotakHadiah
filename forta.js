cd0ac1dabf4f644db5d49c50d25513a0dc0a2f19
Yogs <yogazacty@gmail.com>
2022-09-06 12:22:05 +0700

    Fix: new query

--- forta.js
+++ forta.js
@@ -67,7 +67,7 @@ scannerData.forEach((el) => totalReward += el.rewardPool);
             'content-type': 'application/json',
             'Referer': 'https://explorer.forta.network/'
         },
-        body: JSON.stringify({ query: 'query Retrive {\r\ngetChains {\r\nchainId\r\nupAgents\r\ntotalAgents\r\nupScanners\r\ntotalScanners\r\n}\r\n}' }),
+        body: JSON.stringify({ query: 'query Retrive {\n  getChains {\n    chainId\n    bots\n    scanners\n    __typename\n  }\n}\n' }),
         method: 'POST'
     })
     .then((res) => res.json())
@@ -77,10 +77,11 @@ scannerData.forEach((el) => totalReward += el.rewardPool);
             
     for (let c = 0; c < chainsData.length; c++) {
         const chain = chainsData[c];
+        delete chain.__typename
         Object.assign(scannerData[scannerData.findIndex((el) => el.chainId === chain.chainId)], chain)
     }
-    
-    let parsedData = scannerData.map((x) => `*\\#${x.network}*\nAgents: ${x.upAgents}\nScanners: ${x.upScanners}\nWeekly Reward: ${parseFloat(x.rewardPool / x.totalScanners).toFixed(2)} FORT (Approximately)\nWeekly Reward Pool: ${x.rewardPool} FORT`)
+
+    let parsedData = scannerData.map((x) => `*\\#${x.network}*\nBot: ${x.bots}\nScanners: ${x.scanners}\nWeekly Reward: ${parseFloat(x.rewardPool / x.scanners).toFixed(2)} FORT (Approximately)\nWeekly Reward Pool: ${x.rewardPool} FORT`)
     // eslint-disable-next-line require-unicode-regexp
     let text = `${parsedData.toString().replaceAll(',', '\n\n')}\n\nReward Pool for __Week Ending ${getSundayOfCurrentWeek()}__ is *${totalReward.toLocaleString()}* FORT\n_Rewards per participant will depend on their relative performance to others, measured by an SLA score, and the amount of time in the week which they performed at an above minimum score (0.75). Average SLA will be the average of all the the values which were above minimum score._`.replace(/\./g, '\\.').replace(/\(/g, '\\(').replace(/\)/g, '\\)');
     // eslint-disable-next-line camelcase
